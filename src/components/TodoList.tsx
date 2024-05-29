9128df160332a9eb9ba1345844a086580748efb0
gyuhaaa <vkvkvk2892@gmail.com>
2024-05-29 11:48:45 +0900

    first-commit

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,23 @@
+import { Flex } from "@chakra-ui/react";
+import { FC } from "react";
+import TodoCard from "./TodoCard";
+
+const TodoList: FC = () => {
+  return (
+    <Flex
+      bgColor={"gray.50"}
+      flexGrow={1}
+      flexDir={"column"}
+      alignItems={"center"}
+      pt={8}
+    >
+      <TodoCard />
+      <TodoCard />
+      <TodoCard />
+      <TodoCard />
+      <TodoCard />
+    </Flex>
+  );
+};
+
+export default TodoList;
