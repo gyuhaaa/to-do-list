ff4d71aff2e50018a4e5fd1ddb8ac0c81970df9b
gyuhaaa <vkvkvk2892@gmail.com>
2024-05-29 16:31:12 +0900

    Todo Modify

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,13 +1,15 @@
-import { Button, Flex, Text } from "@chakra-ui/react";
-import { Dispatch, FC, SetStateAction } from "react";
-import { FiEdit3, FiTrash } from "react-icons/fi";
-
+import { Button, Flex, Input, Text } from "@chakra-ui/react";
+import { Dispatch, FC, SetStateAction, useState } from "react";
+import { FiEdit3, FiTrash2, FiX } from "react-icons/fi";
 interface TodoCardProps {
   todo: ITodo;
   todos: ITodo[];
   setTodos: Dispatch<SetStateAction<ITodo[]>>;
 }
 const TodoCard: FC<TodoCardProps> = ({ todo, todos, setTodos }) => {
+  const [isUpdateOpen, setIsUpdateOpen] = useState<boolean>(false);
+  const [content, setContent] = useState<string>("");
+
   const onClickIsDone = () => {
     const temp = todos.map((v) => {
       if (v.id === todo.id) {
@@ -18,6 +20,21 @@ const TodoCard: FC<TodoCardProps> = ({ todo, todos, setTodos }) => {
     });
 
     setTodos(temp);
+  };
+
+  const onClickUpdate = () => {
+    if (!content) return;
+
+    const temp = todos.map((v) => {
+      if (v.id === todo.id) {
+        return { id: todo.id, content, isDone: todo.isDone };
+      } else {
+        return v;
+      }
+    });
+
+    setTodos(temp);
+    setIsUpdateOpen(false);
   };
 
   const onClickDelete = () => {
@@ -32,20 +49,29 @@ const TodoCard: FC<TodoCardProps> = ({ todo, todos, setTodos }) => {
 
   return (
     <Flex bgColor={"white"} px={4} py={2} rounded={"lg"} gap={1}>
-      <Text
-        fontSize={20}
-        w={48}
-        isTruncated={true}
-        textDecorationLine={`${todo.isDone && "line-through"}`}
-        onClick={onClickIsDone}
-      >
-        {todo.content}
-      </Text>
-      <Button colorScheme="blue">
+      {isUpdateOpen ? (
+        <Flex>
+          <Input value={content} onChange={(e) => setContent(e.target.value)} />
+          <Button colorScheme="green" ml={1} onClick={onClickUpdate}>
+            <FiEdit3 />
+          </Button>
+        </Flex>
+      ) : (
+        <Text
+          fontSize={20}
+          w={48}
+          isTruncated={true}
+          textDecorationLine={`${todo.isDone && "line-through"}`}
+          onClick={onClickIsDone}
+        >
+          {todo.content}
+        </Text>
+      )}
+      <Button colorScheme="blue" onClick={() => setIsUpdateOpen(!isUpdateOpen)}>
         <FiEdit3 />
       </Button>
       <Button colorScheme="red" onClick={onClickDelete}>
-        <FiTrash />
+        <FiTrash2 />
       </Button>
     </Flex>
   );
